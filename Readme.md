a77be086ce2a21b10aa8d065506a969df80575b6
Jose <rodriguez.rq67@gmail.com>
2024-10-10 19:57:06 -0300

    inicio projeto jogo

---
+++ Readme.md
@@ -0,0 +1,1 @@
+#jogo d avelha
---
+++ jogo_velha.py
@@ -0,0 +1,41 @@
+# Example file showing a basic pygame "game loop"
+import pygame
+
+# pygame setup
+pygame.init()
+screen = pygame.display.set_mode((500, 500))
+clock = pygame.time.Clock()
+running = True
+cor_fundo = 1
+
+
+while running:
+    # poll for events
+    # pygame.QUIT event means the user clicked X to close your window
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            print('Clicou')
+            cor_fundo = cor_fundo + 1
+            if(cor_fundo > 3):
+                cor_fundo = 1
+
+
+    # fill the screen with a color to wipe away anything from last frame
+
+    # RENDER YOUR GAME HERE
+    if cor_fundo == 1:
+         screen.fill("blue")
+    elif cor_fundo == 2:
+         screen.fill("yellow")
+    else: 
+         screen.fill("#ff6347")
+        
+
+    # flip() the display to put your work on screen
+    pygame.display.flip()
+
+    clock.tick(60)  # limits FPS to 60
+
+pygame.quit()
